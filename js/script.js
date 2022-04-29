d9867bd2deea0512d0db56fe52d143e4ae413b21
temirlan-emls <temirlan.emls@gmail.com>
2022-04-29 14:44:26 +0600

    UI only

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+for (let i = 0; i < 256; i++) {
+    const gridElem = document.createElement('div');
+    gridElem.className = 'grid-elem';
+
+    document.querySelector('.paint-main').append(gridElem);
+}
