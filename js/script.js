3d64d2cd9e1c7ebf6b29fa37e36de788a4f1d505
temirlan-emls <temirlan.emls@gmail.com>
2022-04-29 19:12:01 +0600

    EAS ver1.0

--- js/script.js
+++ js/script.js
@@ -1,7 +1,71 @@
+//Create Grid
 
-for (let i = 0; i < 256; i++) {
-    const gridElem = document.createElement('div');
-    gridElem.className = 'grid-elem';
+let form = document.querySelector(".my-form");
+const paintMain = document.querySelector('.paint-main');
 
-    document.querySelector('.paint-main').append(gridElem);
+function gridGen (cellNum) {
+    for (let i = 0; i < cellNum; i++) {
+        const gridElem = document.createElement('div');
+        gridElem.className = 'grid-elem :hover';
+    
+        paintMain.append(gridElem);
+    }
 }
+function gridDel () {
+    paintMain.innerHTML = '';
+}
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault() // This prevents the window from reloading
+
+    let formdata = new FormData(this);
+    let cells = formdata.get("my-input");
+    let scale = 640/cells;
+    
+
+
+    if (0 <= cells && cells <= 100) {
+        gridDel();
+        gridGen (cells*cells);
+        paint();
+    
+        paintMain.setAttribute('style', `
+            display: grid;
+    
+            grid-template-columns: repeat(${cells}, ${scale}px);
+            grid-template-rows: repeat(${cells}, ${scale}px);
+        `)
+    } else {
+        alert('ERROR! Please enter number between 0 - 100');
+    }
+});
+
+
+// Paint
+
+function paint() {
+    let children = paintMain.children;
+    for (let i = 0; i < children.length; i++) {
+        children[i].addEventListener('mouseenter', (e) => {e.target.classList.add("red")})
+    }
+
+}
+
+//reset 
+
+const resetBtn = document.querySelector('.reset');
+
+resetBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let children = paintMain.children;
+    for (let i = 0; i < children.length; i++) {
+        children[i].classList.remove("red")
+    }
+})
+// choose color 
+
+
+
+gridGen(256);
+paint();
