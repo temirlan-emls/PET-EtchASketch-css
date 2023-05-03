5ac55190058367ac031d97a906456ce3034f2243
Temirlan Emls <temirlan.emls@gmail.com>
2023-05-04 02:30:22 +0600

    ver 1

--- js/script.js
+++ js/script.js
@@ -1,71 +1,121 @@
-//Create Grid
-
-let form = document.querySelector(".my-form");
 const paintMain = document.querySelector('.paint-main');
 
-function gridGen (cellNum) {
-    for (let i = 0; i < cellNum; i++) {
+function gridGen(cellNum) {
+    paintMain.innerHTML = '';
+    for (let i = 0; i < cellNum * cellNum; i++) {
         const gridElem = document.createElement('div');
-        gridElem.className = 'grid-elem :hover';
-    
+        gridElem.classList.add('grid-elem');
+        gridElem.dataset.index = i + 1;
         paintMain.append(gridElem);
     }
+    paintMain.setAttribute(
+        'style',
+        `
+        display: grid;
+        grid-template-columns: repeat(${cellNum}, ${640 / cellNum}px);
+        grid-template-rows: repeat(${cellNum}, ${640 / cellNum}px);`
+    );
 }
-function gridDel () {
-    paintMain.innerHTML = '';
-}
 
-
-form.addEventListener("submit", function (e) {
-    e.preventDefault() // This prevents the window from reloading
-
-    let formdata = new FormData(this);
-    let cells = formdata.get("my-input");
-    let scale = 640/cells;
-    
-
-
-    if (0 <= cells && cells <= 100) {
-        gridDel();
-        gridGen (cells*cells);
-        paint();
-    
-        paintMain.setAttribute('style', `
-            display: grid;
-    
-            grid-template-columns: repeat(${cells}, ${scale}px);
-            grid-template-rows: repeat(${cells}, ${scale}px);
-        `)
-    } else {
-        alert('ERROR! Please enter number between 0 - 100');
-    }
-});
-
-
-// Paint
-
+let tempCellArr = [];
+let cellSet = [];
+let cursorColor = '#e66465';
+const paintHandler = (e) => {
+    e.target.style.backgroundColor = cursorColor;
+    tempCellArr.push(e.target.dataset.index);
+};
 function paint() {
     let children = paintMain.children;
     for (let i = 0; i < children.length; i++) {
-        children[i].addEventListener('mouseenter', (e) => {e.target.classList.add("red")})
+        children[i].addEventListener('mouseenter', paintHandler);
+    }
+    tempCellArr = [];
+    cellSet.push(tempCellArr);
+}
+function unPaint() {
+    let children = paintMain.children;
+    for (let i = 0; i < children.length; i++) {
+        children[i].removeEventListener('mouseenter', paintHandler);
+    }
+}
+paintMain.addEventListener('mousedown', () => {
+    paint();
+});
+paintMain.addEventListener('mouseup', () => {
+    unPaint();
+});
+
+let backButton = document.querySelector('.back');
+backButton.addEventListener('click', () => {
+    if (cellSet.length) {
+        let lastDraw = cellSet[cellSet.length - 1].sort();
+        let children = paintMain.children;
+        let temp = 0;
+        for (let i = 0; i < children.length; i++) {
+            if (lastDraw[temp] === children[i].dataset.index) {
+                children[i].style.backgroundColor = 'white';
+                temp++;
+            }
+        }
+        cellSet.pop();
+    } else {
+        paintMain.innerHTML = '';
+        gridGen(64);
+        alert('Draw something!');
+    }
+});
+
+const cellNumber = document.querySelector('.my-form');
+cellNumber.addEventListener('submit', function (e) {
+    e.preventDefault(); // This prevents the window from reloading
+    let formdata = new FormData(this);
+    let cells = formdata.get('my-input');
+
+    if (64 <= cells && cells <= 128) {
+        gridGen(cells);
+    } else {
+        alert('ERROR! Please enter number between 64 - 128');
+    }
+});
+
+let recentColors = [];
+const cellColor = document.querySelector('.my-color');
+const recentColorsB = document.querySelectorAll('.recentColor');
+cellColor.addEventListener('submit', (e) => {
+    e.preventDefault(); // This prevents the window from reloading
+
+    cursorColor = e.target.children[0].value;
+    if (!recentColors.includes(cursorColor)) {
+        if (recentColors.length < 6) {
+            recentColors.push(cursorColor);
+        } else {
+            recentColors.shift();
+            recentColors.push(cursorColor);
+        }
     }
 
-}
+    recentColorsB.forEach((item, index) => {
+        item.style.backgroundColor = recentColors[index];
+        item.dataset.color = recentColors[index];
+    });
+});
+recentColorsB.forEach((el) => {
+    el.addEventListener('click', () => {
+        if (el.dataset.color && el.dataset.color !== undefined) {
+            cursorColor = el.dataset.color;
+            cellColor.children[0].value = el.dataset.color;
+        }
+    });
+});
 
-//reset 
-
+//reset
 const resetBtn = document.querySelector('.reset');
-
 resetBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let children = paintMain.children;
     for (let i = 0; i < children.length; i++) {
-        children[i].classList.remove("red")
+        children[i].style.backgroundColor = 'white';
     }
-})
-// choose color 
+});
 
-
-
-gridGen(256);
-paint();
+gridGen(64);
